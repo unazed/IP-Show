a3532bfdb2ff26e7d3c871642a65c355d079a839
Noah Cristino <noahcristino@yahoo.com>
2016-11-24 21:32:17 -0500

    Commented Code

--- src/ip.py
+++ src/ip.py
@@ -1,4 +1,4 @@
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(('google.com', 0))
+s.connect(('google.com', 0)) #you can change this website! default is google but cnn is recommended because it changes all the time!
 print "Your IP is: "+s.getsockname()[0]
